b9c9e34077f39ad12595f6ede14cb7e0f9a41aa4
jiyong <wldyd2113@gmail.com>
2026-03-25 00:14:26 +0900

    Creat FitAlly

---
+++ FitAlly/FitAlly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitAlly
+//
+//  Created by 차지용 on 3/25/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
